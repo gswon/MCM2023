75d7626a9e0e763ad8770f29268a14a51c254e96
Gangwon Suh <117620824+gswon@users.noreply.github.com>
2023-02-20 20:19:26 -0500

    Update count-repeated-letter-ofword.py

--- count-repeated-letter-ofword.py
+++ count-repeated-letter-ofword.py
@@ -1,13 +1,38 @@
-list_word = list(set)
+from english_words import get_english_words_set
+lst = get_english_words_set(['web2'], lower=True)
+lst = list(lst)
 
 count = 0
-for i in range(len(list_word)):
-    word = sorted(list_word[i])
-    first_letter = word[0]
-    for j in range(1, len(word)):
-        if first_letter == word[j]:
+num = 0
+for i in lst:
+    i = list(i)
+    i = sorted(i)
+    if len(i) == 5:
+        if i[-1] == i[-2]:
             count += 1
-            break
         else:
-            first_letter = word[j]
+            count += 0
+
+    else:
+        count += 0
+
+for j in lst:
+    if len(j) == 5:
+        num += 1
+    else:
+        num += 0
+
+
 print(count)
+print(num)
+print(f" The number of repeated letter of words{count/num * 100}")
+
+
+
+lst2 = get_english_words_set(['web2'], lower=True)
+lst2 = list(lst2)
+count = 0
+for i in lst2:
+    if "a" in i:
+        count += 1
+print(f"Calculating word with a certain letter: {count/len(lst2) * 100}")
